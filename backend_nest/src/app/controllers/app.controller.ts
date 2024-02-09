da3c66a094eaab9cff489907fef90c209d76470f
KSoft-Solution <kanhasahu955902@gmail.com>
2024-02-10 01:05:16 +0530

    nest api setup

---
+++ backend_nest/src/app/controllers/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Request } from '@nestjs/common';
+import { AppService } from '../services/app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  root(@Request() req): string {
+    return this.appService.root();
+  }
+}
--- backend_nest/src/main.ts
+++ backend_nest/src/main.ts
@@ -1,8 +1,33 @@
+import 'dotenv/config';
+import _ from 'lodash';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
+import { AppModule } from './app/app.module';
+
+const PORT: any = process.env.PORT || 5000;
+declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  setupSwagger(app, PORT);
+  await app.listen(PORT);
+  Logger.log(`Server running on http://localhost:${PORT}`, 'main');
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
